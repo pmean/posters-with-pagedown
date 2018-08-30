d28533a78065f4b5e4af3015d0a162bd000a9b49
Steve Simon <mail@pmean.com>
2018-08-30 14:38:53 -0500

    Added move-output-to-results-folder.R

---
+++ src/move-output-to-results-folder.R
@@ -0,0 +1,9 @@
+project_name <- getwd()
+src <- paste(project_name, "src", sep="/")
+results <- paste(project_name, "results", sep="/")
+html_list <- list.files(src, pattern="*.html")
+for (i in html_list) {
+  file.copy(paste(src, i, sep="/"), results, overwrite=TRUE)
+  file.remove(paste(src, i, sep="/"))
+}
+cat("\n\n", length(html_list), " file(s) moved: ", paste(html_list, collapse=", "), sep="")
